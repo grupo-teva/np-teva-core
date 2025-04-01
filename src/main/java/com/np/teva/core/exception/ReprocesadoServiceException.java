d26b526ea445473ffe8dfc4ecd2b8fb681b65780
jariquelme77 <juanantonioriquelme@grupoteva.com>
2025-04-01 16:42:49 +0200

    FUE-60: INTERFACES - Sanciones - Generación de remesado de boletines.
    lanzamos reprocesarQC antes de remesar

---
+++ src/main/java/com/np/teva/core/exception/ReprocesadoServiceException.java
@@ -0,0 +1,15 @@
+package com.np.teva.core.exception;
+
+public class ReprocesadoServiceException extends Exception {
+    public ReprocesadoServiceException() {
+        super();
+    }
+
+    public ReprocesadoServiceException(String message) {
+        super(message);
+    }
+
+    public ReprocesadoServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
